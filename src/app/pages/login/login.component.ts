e7485835531c714c36ed0ab5a10ecb3c080d02c1
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2019-04-02 18:49:27 +0200

    Páginas de login y registro

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+import { LoginData }         from '../../interfaces/interfaces';
+import { ApiService }        from '../../services/api.service';
+import { UserService }       from '../../services/user.service';
+import { CommonService }     from '../../services/common.service';
+import { DataShareService }  from '../../services/data-share.service';
+import { AuthService }       from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './html/login.component.html',
-  styleUrls: ['./css/login.component.css']
+  styleUrls: []
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+	loginData = {
+		name: '',
+		pass: ''
+	} as LoginData;
+	loginError: boolean = false;
+	loginSending: boolean = false;
+
+	constructor(private as: ApiService,
+				private user: UserService,
+				private cs: CommonService,
+				private router: Router,
+				private dss: DataShareService,
+				private auth: AuthService) {}
+	ngOnInit() {
+		if (this.auth.isAuthenticated()){
+			this.router.navigate(['/home']);
+		}
+	}
+
+	doLogin(ev) {
+		ev.preventDefault();
+
+		if (this.loginData.name==='' || this.loginData.pass===''){
+			return false;
+		}
+
+		this.loginSending = true;
+		this.as.login(this.loginData).subscribe(result => {
+			this.loginSending = false;
+			if (result.status==='ok'){
+				this.user.logged = true;
+				this.user.id     = result.id;
+				this.user.name   = this.cs.urldecode(result.name);
+				this.user.token  = this.cs.urldecode(result.token);
+				this.user.saveLogin();
+
+				this.router.navigate(['/main']);
+			}
+			else{
+				this.loginError = true;
+			}
+		});
+	}
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+import { RegisterData }      from '../../interfaces/interfaces';
+import { ApiService }        from '../../services/api.service';
+import { UserService }       from '../../services/user.service';
+import { CommonService }     from '../../services/common.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './html/register.component.html',
-  styleUrls: ['./css/register.component.css']
+  styleUrls: []
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+	registerData = {
+		name: '',
+		pass: '',
+		conf: ''
+	} as RegisterData;
+	registerNameError: boolean = false;
+	registerPassError: boolean = false;
+	registerSending: boolean = false;
+
+	constructor(private as: ApiService, private user: UserService, private cs: CommonService, private router: Router) {}
+	ngOnInit() {}
+	
+	doRegister(ev) {
+		ev.preventDefault();
+		
+		if (this.registerData.name==='' || this.registerData.pass==='' || this.registerData.conf===''){
+			return false;
+		}
+		
+		this.registerNameError = false;
+		this.registerPassError = false;
+		if (this.registerData.pass !== this.registerData.conf){
+			this.registerPassError = true;
+			return false;
+		}
+		
+		this.registerSending = true;
+		this.as.register(this.registerData).subscribe(result => {
+			this.registerSending = false;
+			if (result.status==='ok'){
+				this.user.logged = true;
+				this.user.id     = result.id;
+				this.user.name   = this.cs.urldecode(result.name);
+				this.user.token  = this.cs.urldecode(result.token);
+				this.user.saveLogin();
+				
+				this.router.navigate(['/home']);
+			}
+			else{
+				this.registerNameError = true;
+			}
+		});
+	}
 }

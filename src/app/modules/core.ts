44ac1033156c78b7c2608728896bf7b377f0fa29
Iñigo Gorosabel <inigo.gorosabel@idom.com>
2025-05-26 12:41:05 +0200

    Actualizo paquetes de linting y quito DataShareService

--- src/app/modules/core.ts
+++ src/app/modules/core.ts
@@ -2,16 +2,9 @@ import { Provider } from "@angular/core";
 import ApiService from "@services/api.service";
 import AuthService from "@services/auth.service";
 import ClassMapperService from "@services/class-mapper.service";
-import DataShareService from "@services/data-share.service";
 import UserService from "@services/user.service";
 
 function provideCore(): Provider[] {
-  return [
-    ApiService,
-    AuthService,
-    ClassMapperService,
-    DataShareService,
-    UserService,
-  ];
+  return [ApiService, AuthService, ClassMapperService, UserService];
 }
 export default provideCore;
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,34 +1,30 @@
-import { inject, Injectable } from "@angular/core";
-import { UserInterface } from "@interfaces/interfaces";
+import { Injectable } from "@angular/core";
 import User from "@model/user.model";
-import DataShareService from "@services/data-share.service";
 
 @Injectable({
   providedIn: "root",
 })
 export default class UserService {
-  private dss: DataShareService = inject(DataShareService);
-
   logged: boolean = false;
   user: User = null;
 
   loadLogin(): void {
-    const loginObj: UserInterface = this.dss.getGlobal("login");
+    const loginObj: string = localStorage.getItem("login");
     if (loginObj === null) {
       this.logout();
     } else {
       this.logged = true;
-      this.user = new User().fromInterface(loginObj);
+      this.user = new User().fromInterface(JSON.parse(loginObj));
     }
   }
 
   saveLogin(): void {
-    this.dss.setGlobal("login", this.user.toInterface());
+    localStorage.setItem("login", JSON.stringify(this.user.toInterface()));
   }
 
   logout(): void {
     this.logged = false;
     this.user = null;
-    this.dss.removeGlobal("login");
+    localStorage.removeItem("login");
   }
 }

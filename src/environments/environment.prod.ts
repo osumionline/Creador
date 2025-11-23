42db177016aa18d6dacf74bb4f8a2a8694d9e45a
Osumi <admin@osumi.es>
2025-11-24 00:22:41 +0100

    Actualizo Angular CLI (20.3.10 -> 21.0.0), Angular (20.3.12 -> 21.0.0), Angular Material (20.2.13 -> 21.0.0), Angular ESLint (20.6.0 -> 21.0.1) y Osumi Angular Tools (1.2.3 -> 1.3.0)

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "https://apicreador.osumi.dev/api/",
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: false,
-  apiUrl: "https://apicreador.osumi.dev/api/",
+  apiUrl: 'https://apicreador.osumi.dev/api/',
 };
